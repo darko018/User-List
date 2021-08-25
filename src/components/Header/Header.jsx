8cab4cd46ea0651c0a23c2f150642746fe8ff764
Darko Kovačević <darkokovacevic018@gmail.com>
2021-08-25 11:39:18 +0200

    'project finish'

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from "react"
 import "./Header.css"
 
 const Header = ({isListView, onLayoutChange }) => {
-    console.log('Header: ', isListView);
+   
     return (
         <div className="header">
                 <p>React Users</p>
